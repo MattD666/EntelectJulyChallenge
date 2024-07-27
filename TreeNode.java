7e4a21cbc6e49059ddb824672203c19ce778e031
Caleb Bessit <99912548+CalebBessit@users.noreply.github.com>
2024-07-27 11:18:52 +0200

    Add files via upload

---
+++ TreeNode.java
@@ -0,0 +1,53 @@
+/**
+
+Node for a general tree
+@author Caleb Bessit
+@version 16 July 2024
+
+**/
+
+import java.util.ArrayList;
+
+public class TreeNode
+{
+   
+   private float x,y;
+   private int biomeIndex;
+   private float quality;
+   private float value;
+   
+   private int[] biomeValues = {1,14,28,42,57,71,85,100};
+   
+   private TreeNode top, bottom, left, right;
+   
+   public TreeNode(){
+      this.top = null;
+      this.bottom = null;
+      this.left = null;
+      this.right = null;
+
+   }
+   
+   public TreeNode(float x, float y, int biomeIndex, float quality, TreeNode top, TreeNode bottom, TreeNode left, TreeNode right){
+      this.x = x;
+      this.y = x;
+      this.biomeIndex = biomeIndex;
+      this.quality = quality;
+      this.top = top;
+      this.bottom = bottom;
+      this.left = left;
+      this.right = right;
+      
+   }
+   
+
+  
+   public float value(){
+      return this.biomeValues[this.biomeIndex]*this.quality;
+   }
+      
+
+
+    
+   
+}

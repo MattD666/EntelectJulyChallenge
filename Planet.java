034558e721d991f2a7a44cd863a64da0d27fa87b
Caleb Bessit <99912548+CalebBessit@users.noreply.github.com>
2024-07-27 13:28:50 +0200

    Latest planet

--- Planet.java
+++ Planet.java
@@ -3,14 +3,21 @@ import java.util.List;
 import java.io.File;
 import java.util.Scanner;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 public class Planet
 {
 
-   TreeNode northPole;
-   TreeNode southPole;
+   TreeNode northPole, southPole;
    int radius = 10;
+   int days = 37;
+   int steps  = 3*this.days;
    ArrayList<TreeNode> nodes;
    ArrayList<TreeNode> northChildren;
+   String path = "";
    
   
    /**
@@ -18,21 +25,94 @@ public class Planet
    **/
    public Planet(){
       nodes = new ArrayList<TreeNode>();
+      
+      //READ IN FILE
+      
+      try {
+            File myObj = new File("1.txt");
+            Scanner myReader = new Scanner(myObj);  
+
+            while (myReader.hasNextLine()) {
+
+                String data = myReader.nextLine();
+
+                // print line
+
+
+                // process line
+                data = data.substring(1, data.length()-1);
+                String[] myArray = data.split(";");
+
+                String coords = myArray[0];
+                coords = coords.substring(1, coords.length()-1);
+                String[] myCoords = coords.split(",");
+
+                int xCoord = Integer.parseInt(myCoords[0]);
+                int yCoord = Integer.parseInt(myCoords[1]);
+
+
+
+                // check if not first or last lines
+                if(myArray.length > 1){
+                    int biome = Integer.parseInt(myArray[1]);
+                    // System.out.println("Biome:\t\t" + (biome+1-1));
+                    float quality = Float.parseFloat(myArray[2]);
+                    // System.out.println("Quality:\t" + (quality+1-1));
+                    
+
+                    nodes.add( new TreeNode(xCoord, yCoord, biome, quality)  );
+                }
+                else{
+                  nodes.add( new TreeNode(xCoord, yCoord, -1, -1)  );
+                }
+                
+                
+
+
+            }
+
+            myReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+      
+      
+      //END READ IN FILE
+      
+      System.out.println(nodes);
       northChildren = new ArrayList<TreeNode>();
       
-
-      northPole = nodes.get(0);
-      southPole = nodes.get(nodes.size()-1);
+     northPole = nodes.get(0);
+     southPole = nodes.get( nodes.size()-1 );
       
+      
+      
+        int start = -1;
+      int end = -1;
       
       for (int i=0; i<(radius*2+1); i++){
-         constructLevel(i*(radius*2+1) + 1, (i+1)*(radius*2+1));
+         start = i*(radius*2+1) + 1;
+         end = (i+1)*(radius*2+1);
+         this.constructLevel(start, end);
       }
-   }
+      
+    
+
+    //     int testIndex = 1;
+//         System.out.println("Node (-10,10) left and right");
+//         System.out.println(nodes.get(testIndex).toString());
+//         System.out.println(nodes.get(testIndex).getLeft());   
+//         System.out.println(nodes.get(testIndex).getRight()); 
+//         System.out.println(nodes.get(testIndex).getTop());   
+//         System.out.println(nodes.get(testIndex).getBottom()); 
+      }
+   
    
    public void constructLevel(int start, int end){
       
-      for (int i=start; i<end; i++){
+      for (int i=start; i<=end; i++){
          TreeNode current = nodes.get(i);
          if (current.getX()==this.radius ){
             current.setLeft(nodes.get(i-1));
@@ -62,8 +142,64 @@ public class Planet
       }
       
    }
+
    
+   public void traverse(){
+      northPole.visit();
+      this.path = this.path + northPole.toString();
+      
+      TreeNode bestNode = northChildren.get(0);
+      
+      float maxvalue = bestNode.value();
+      for (int i=1; i<northChildren.size(); i++){
+         if (northChildren.get(i).value()>maxvalue){
+            bestNode = northChildren.get(i);
+         }
+      }
+      
+      System.out.println(bestNode);
+      System.out.println(path);
+      this.traverse(bestNode);
+      
    
+   }
+   
+   public void traverse(TreeNode node){
+      if (node==this.southPole){
+         return;
+      }
+      if ( (node.getY()-this.southPole.getY()) == this.steps  ){
+         this.goToSouthPole(node);
+      }
+      else{
+      //Not going directly to south pole
+      
+         TreeNode bestNode = node.checkSurroundingNodes(); //Replace with method
+         
+         node.visit();
+         this.path = this.path +  ","+node.toString();
+         this.traverse(bestNode);
+         System.out.println(path);
+      
+      }//END Not going directly to south pole
+   
+   }
+   
+   public void goToSouthPole(TreeNode node){
+      if ( node==this.southPole ){
+         return;
+      }  
+      else{
+         this.path = this.path +  ","+node.toString();
+         node.visit();
+         this.goToSouthPole(node.getBottom() );
+      } 
+   }
+   
+   public String getPath(){
+   
+      return "[" + this.path + "]";
+   }
    
                
 }
